a5eb9b5835006c86d1aff5cf76c3e6e637c83bff
Santiago Caballero <saantyc12.caballero@gmail.com>
2023-03-13 12:32:27 -0300

    add loader

--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../context/authContext';
 
+import styles from './index.module.css';
+
 interface PropsChildren {
 	children: React.ReactNode;
 }
@@ -9,8 +11,23 @@ interface PropsChildren {
 const ProtectedRoute = ({ children }: PropsChildren): React.ReactElement => {
 	const { loading } = useAuth();
 
-	if (loading !== false) {
-		return <h1>Cargando...</h1>;
+	if (loading) {
+		return (
+			<div className={styles.contLoaderCol}>
+				<div className={styles.contContentLoader}>
+					<div className={styles.contSpinner}>
+						<div className={styles.loadSpinner}></div>
+					</div>
+					<h1>Loading...</h1>
+				</div>
+				{/* <div className={styles.contLoaderCol}>
+					<div className={styles.contSpinner}>
+						<div className={styles.loadSpinner}></div>
+					</div>
+					<h1>Cargando...</h1>
+				</div> */}
+			</div>
+		);
 	}
 
 	if (localStorage.getItem('user_token') === null) {
